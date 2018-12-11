6aa793f0f37f4e27ac74771eb73a6dd6c81eaa31
Martin Holst Swende <martin@swende.se>
2018-12-11 11:48:01 +0100

    puppeth: add progpow to parity chain spec

--- cmd/puppeth/genesis.go
+++ cmd/puppeth/genesis.go
@@ -214,6 +214,7 @@ type parityChainSpec struct {
 				DifficultyBombDelays   map[string]string `json:"difficultyBombDelays"`
 				HomesteadTransition    hexutil.Uint64    `json:"homesteadTransition"`
 				EIP100bTransition      hexutil.Uint64    `json:"eip100bTransition"`
+				ProgpowTransition      *hexutil.Uint64   `json:"progpowTransition,omitempty"`
 			} `json:"params"`
 		} `json:"Ethash"`
 	} `json:"engine"`
@@ -342,6 +343,11 @@ func newParityChainSpec(network string, genesis *core.Genesis, bootnodes []strin
 	// Constantinople
 	if num := genesis.Config.ConstantinopleBlock; num != nil {
 		spec.setConstantinople(num)
+	}
+	// Progpow
+	if num := genesis.Config.ProgpowBlock; num != nil {
+		hexnum := hexutil.Uint64(num.Uint64())
+		spec.Engine.Ethash.Params.ProgpowTransition = &hexnum
 	}
 	spec.Params.MaximumExtraDataSize = (hexutil.Uint64)(params.MaximumExtraDataSize)
 	spec.Params.MinGasLimit = (hexutil.Uint64)(params.MinGasLimit)
